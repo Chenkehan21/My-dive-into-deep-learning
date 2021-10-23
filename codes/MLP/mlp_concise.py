d5bd316522b4c1cf8098f01eb3b2bfc8583b479c
Chenkehan21 <56097561+Chenkehan21@users.noreply.github.com>
2021-10-23 13:27:39 +0800

    Add files via upload

---
+++ codes/MLP/mlp_concise.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append('..')
+
+from utils.utils import load_FMNIST, train_FMNIST
+
+import torch
+import torch.nn as nn
+
+
+class MLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(784, 256),
+            nn.ReLU(),
+            nn.Linear(256, 10)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+def init_weights(layer):
+    if type(layer) == nn.Linear:
+        nn.init.normal_(layer.weight, mean=0, std=0.01)
+
+
+def main(batch_size=256, lr=1e-1, epochs=10, num_workers=8):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net = MLP().to(device)
+    net.apply(init_weights)
+    train_data, val_data = load_FMNIST(batch_size, num_workers)
+    train_FMNIST(net, train_data, val_data, lr, epochs, fig_name="mlp_concise")
+
+
+if __name__ == "__main__":
+    main()
---
+++ codes/MLP/mlp_from_scratch.py
@@ -0,0 +1,73 @@
+import sys
+sys.path.append('..')
+
+from utils.utils import load_FMNIST, right_prediction, val_acc
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+
+def init_weights():
+    w1 = torch.normal(0, 0.1, (784, 256), requires_grad=True)
+    b1 = torch.zeros(256, requires_grad=True)
+    w2 = torch.normal(0, 0.1, (256, 10), requires_grad=True)
+    b2 = torch.zeros(10, requires_grad=True)
+
+    return [w1, w2], [b1, b2]
+
+
+def ReLU(x):
+    z = torch.zeros_like(x)
+    return torch.max(x, z)
+
+
+def mlp(x, w:list, b:list):
+    x = x.reshape(-1, 784)
+    h = ReLU(torch.matmul(x, w[0]) + b[0])
+    return torch.matmul(h, w[1]) + b[1]
+
+
+def train(batch_size=256, lr=1e-1, epochs=10, num_workers=8):
+    train_iter, val_iter = load_FMNIST(batch_size, num_workers)
+    w, b = init_weights()
+    net = mlp
+    # here we use API, our sgd and cross_entropy may encounter many problems such as bad numerical stability
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(params=w + b, lr=lr)
+    train_accs, val_accs, train_losses = [], [],[]
+
+    for epoch in range(epochs):
+        right_cnt, sample_cnt, total_loss, n = 0, 0, 0, 0
+        for feature, label in train_iter:
+            n += 1
+            y_hat = net(feature, w, b)
+            right_cnt += right_prediction(y_hat, label)
+            sample_cnt += len(y_hat)
+            loss = loss_function(y_hat, label)
+            total_loss += loss
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            
+        acc = val_acc(net, val_iter, w, b)
+        train_acc = right_cnt / sample_cnt
+        val_accs.append(acc)
+        train_accs.append(train_acc)
+        train_loss = total_loss / n
+        train_losses.append(train_loss)
+        print("epoch: ", epoch + 1, "| train_loss: %.3f"%train_loss, 
+        "| train_acc: %.3f"%train_acc, "| val_acc: %.3f"%acc)
+    
+    steps = list(range(epochs))
+    plt.plot(steps, train_accs, label='train_acc')
+    plt.plot(steps, val_accs, label='val_acc')
+    plt.plot(steps, train_losses, label='train_loss')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("./mlp_from_scratch2.png")
+
+
+if __name__ == "__main__":
+    train()
