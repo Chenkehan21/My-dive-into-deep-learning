aff9322c1dd6c5fa92d1b424e93895211e8eb166
Chenkehan21 <751722804@qq.com>
2021-11-15 21:43:41 +0800

    update

--- codes/Modern_Neural_Networks/ResNet.py
+++ codes/Modern_Neural_Networks/ResNet.py
@@ -1,6 +1,9 @@
 import sys
 sys.path.append('../')
 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = '2'
+
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -9,18 +12,26 @@ from utils.utils import load_FMNIST, train_FMNIST
 
 
 class Residual(nn.Module):
-    def __init__(self, in_channels, out_channels, use_1x1conv=False) -> None:
+    def __init__(self, in_channels, out_channels, use_1x1conv=False, stride=1) -> None:
         super().__init__()
         self.use_1x1conv = use_1x1conv
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=1)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=1)
-        if use_1x1conv:
-            self.conv_1x1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 
+                               kernel_size=3, padding=1, stride=stride)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 
+                               kernel_size=3, padding=1, stride=1)
+        self.conv_1x1 = nn.Conv2d(in_channels, out_channels,
+                                  kernel_size=1, stride=stride)
         self.relu = nn.ReLU(),
-        self.bn = nn.BatchNorm2d(out_channels)
+
+        '''
+        a very import point! bn1 and bn2 actually have different parameters to learn
+        so it's wrong to use just one bn! Otherwise it will overfit!!!!
+        '''
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
         
     def forward(self, x):
-        y = self.bn(self.conv2(F.relu(self.bn(self.conv1(x)))))
+        y = self.bn2(self.conv2(F.relu(self.bn1(self.conv1(x)))))
         if self.use_1x1conv:
             x = self.conv_1x1(x)
         y += x
@@ -29,14 +40,13 @@ class Residual(nn.Module):
         return y
     
 
-def resdiual_block(in_channels, out_channels, use_1x1conv, num_blocks):
+def resdiual_block(in_channels, out_channels, first_block=False, num_blocks=2):
     block = []
-    for _ in range(num_blocks):
-        block.append(
-            nn.Sequential(
-                Residual(in_channels, out_channels, use_1x1conv)
-            )
-        )
+    for i in range(num_blocks):
+        if i == 0 and not first_block:
+            block.append(Residual(in_channels, out_channels, True, 2))
+        else:
+            block.append(Residual(out_channels, out_channels))
     
     return block
     
@@ -50,22 +60,14 @@ class ResNet(nn.Module):
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         )
         
-        self.block2 = nn.Sequential(*resdiual_block(64, 64, False, 2))
-        self.block3 = nn.Sequential(
-            Residual(64, 64, True),
-            Residual(64, 64, False)
-        )
-        self.block4 = nn.Sequential(
-            Residual(64, 64, True),
-            Residual(64, 64, False)
-        )
-        self.block5 = nn.Sequential(
-            Residual(64, 64, True),
-            Residual(64, 64, False)
-        )
+        self.block2 = nn.Sequential(*resdiual_block(64, 64, True, 2))
+        self.block3 = nn.Sequential(*resdiual_block(64, 128, False, 2))
+        self.block4 = nn.Sequential(*resdiual_block(128, 256, False, 2))
+        self.block5 = nn.Sequential(*resdiual_block(256, 512, False, 2))
         self.block6 = nn.Sequential(
             nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Flatten()
+            nn.Flatten(), 
+            nn.Linear(512, 10)
         )
         
         self.net = nn.Sequential(
@@ -75,17 +77,17 @@ class ResNet(nn.Module):
             self.block4, 
             self.block5,
             self.block6,
-            nn.Linear(64, 10)
         )
         
     def forward(self, x):
         return self.net(x)
     
 
-def main(batch_size=64, lr=0.1, epochs=10):
+def main(batch_size=256, lr=0.05, epochs=10):
     train_data, val_data = load_FMNIST(batch_size, resize=96)
     net = ResNet().net
     print(net)
+    # exit()
     device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
     print("start train")
     train_FMNIST(net, train_data, val_data, device, lr, epochs, "ResNet")
---
+++ codes/Modern_Neural_Networks/resnet_teacher_version.py
@@ -0,0 +1,74 @@
+import sys
+sys.path.append('../')
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = '2'
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from utils.utils import load_FMNIST, train_FMNIST
+
+class Residual(nn.Module):  #@save
+    def __init__(self, input_channels, num_channels,
+                 use_1x1conv=False, strides=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, num_channels,
+                               kernel_size=3, padding=1, stride=strides)
+        self.conv2 = nn.Conv2d(num_channels, num_channels,
+                               kernel_size=3, padding=1)
+        if use_1x1conv:
+            self.conv3 = nn.Conv2d(input_channels, num_channels,
+                                   kernel_size=1, stride=strides)
+        else:
+            self.conv3 = None
+        self.bn1 = nn.BatchNorm2d(num_channels)
+        self.bn2 = nn.BatchNorm2d(num_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        Y += X
+        return F.relu(Y)
+
+
+def resnet_block(input_channels, num_channels, num_residuals,
+                 first_block=False):
+    blk = []
+    for i in range(num_residuals):
+        if i == 0 and not first_block:
+            blk.append(Residual(input_channels, num_channels,
+                                use_1x1conv=True, strides=2))
+        else:
+            blk.append(Residual(num_channels, num_channels))
+    return blk
+
+
+b1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+                   nn.BatchNorm2d(64), nn.ReLU(),
+                   nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+b2 = nn.Sequential(*resnet_block(64, 64, 2, first_block=True))
+b3 = nn.Sequential(*resnet_block(64, 128, 2))
+b4 = nn.Sequential(*resnet_block(128, 256, 2))
+b5 = nn.Sequential(*resnet_block(256, 512, 2))
+
+
+net = nn.Sequential(b1, b2, b3, b4, b5,
+                    nn.AdaptiveAvgPool2d((1,1)),
+                    nn.Flatten(), nn.Linear(512, 10))
+
+
+def main(batch_size=256, lr=0.05, epochs=10):
+    train_data, val_data = load_FMNIST(batch_size, resize=96)
+    print(net)
+    exit()
+    device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+    print("start train")
+    train_FMNIST(net, train_data, val_data, device, lr, epochs, "ResNet")
+
+
+if __name__ == '__main__':
+    main()
