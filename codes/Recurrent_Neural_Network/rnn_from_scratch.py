f4f70ac58003653e99fb3795e13cdd81f73840e7
Chenkehan21 <751722804@qq.com>
2021-11-21 15:57:42 +0800

    update

--- codes/Recurrent_Neural_Network/rnn_from_scratch.py
+++ codes/Recurrent_Neural_Network/rnn_from_scratch.py
@@ -22,9 +22,15 @@ class RNN:
         self.params = [self.w_xh, self.w_hh, self.b_h, self.w_hq, self.b_q]
 
     def __call__(self, x_batch, hidden_state):
-        # x_batch.shape=[batch_size, num_steps]
+        # x_batch.shape=[batch_size, num_steps] (num_steps is the length of sequence)
         res = []
         H = hidden_state
+        '''
+        We often transpose the input so that we will obtain an output of shape 
+        (number of time steps, batch size, vocabulary size). This will allow us 
+        to more conveniently loop through the outermost dimension for updating 
+        hidden states of a minibatch, time step by time step.
+        '''
         X = F.one_hot(x_batch.T, self.vocab_size).type(torch.float32) # X.shape=[num_steps, batch_size, len(vocab)]
         for x in X:
             x = x.to(self.device) # x.shape=[batch_size, len(vocab)]
@@ -51,7 +57,7 @@ def grad_clip(net, theta):
             param.grad *= theta / norm
 
 
-def predit(net, text, predict_steps, vocab, device):
+def predict(net, text, predict_steps, vocab, device):
     hidden_state = net.hidden_state_init(batch_size=1)
     res = [vocab[text[0]]]
     get_data = lambda: torch.tensor(res[-1], device=device).reshape(1, 1)
@@ -65,14 +71,9 @@ def predit(net, text, predict_steps, vocab, device):
     return ''.join(vocab.to_tokens(i) for i in res)
 
 
-def train(batch_size=32, lr=0.5, epochs=500, step=35, use_random_sample=False, token='char', plot=False):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # device = torch.device('cpu')
-    data_iter, vocab = load_data_time_machine(batch_size, step, use_random_sample, token)
-    net = RNN(len(vocab), 512, device)
-    loss_fun = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.params, lr)
-
+def train(epochs, data_iter, vocab, net, loss_fun, optimizer, 
+          device, need_to_clip=False,
+          fig_name=None, need_to_predict=False):
     total_perplexity = []
     for epoch in range(epochs):
         total_loss, n = 0, 0
@@ -107,9 +108,10 @@ def train(batch_size=32, lr=0.5, epochs=500, step=35, use_random_sample=False, t
             '''
             y = y.T.reshape(-1)
             x, y = x.to(device), y.to(device)
-            loss = loss_fun(output, y)
+            loss = loss_fun(output, y).mean()
             loss.backward()
-            grad_clip(net, 1.0)
+            if need_to_clip:
+                grad_clip(net, 1.0)
             total_loss += loss * y.numel()
             n += y.numel()
             optimizer.step()
@@ -118,16 +120,31 @@ def train(batch_size=32, lr=0.5, epochs=500, step=35, use_random_sample=False, t
         total_perplexity.append(perplexity)
         print("epoch: %d|train_loss: %.3f" % (epoch + 1, perplexity))
     
-    if plot:
+    if fig_name is not None:
         x = list(range(len(total_perplexity)))
         plt.plot(x, total_perplexity, label='train')
         plt.legend()
         plt.grid(True)
-        plt.savefig('./images/rnn_from_scratch_grad_clip.png')
+        plt.savefig('./images/%s.png' % fig_name)
+    
+    if need_to_predict:
+        predict_text = predict(net, 'time traveller ', 50, vocab, device)
+        print(predict_text)
 
-    # predict_text = predit(net, 'time traveller ', 2, vocab, device)
-    # print(predict_text)
+
+def main(batch_size=32, lr=0.5, epochs=500, 
+        step=35, use_random_sample=False, token='char', 
+        need_to_clip=False, plot=False, need_to_predict=False):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # device = torch.device('cpu')
+    data_iter, vocab = load_data_time_machine(batch_size, step, use_random_sample, token)
+    net = RNN(len(vocab), 512, device)
+    loss_fun = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.params, lr)
+
+    train(epochs, data_iter, vocab, net, loss_fun, optimizer, 
+          device, need_to_clip, fig_name, need_to_predict)
 
 
 if __name__ == "__main__":
-    train(plot=True)
+    main()
